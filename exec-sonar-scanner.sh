2995bca980656dec8cbc33d787c0c9da8ff7e709
Terry Cox <terry@bootstrap.je>
2020-04-07 23:22:54 +0100

    fix: Clean up batch script
    Signed-off-by: Terry Cox <terry@bootstrap.je>

--- exec-sonar-scanner.sh
+++ exec-sonar-scanner.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 while getopts s:k:r:p: option
 do
 case "${option}"
@@ -6,6 +7,7 @@ s) export SONARQUBE_SERVER=${OPTARG};;
 k) export SONAR_TOKEN=${OPTARG};;
 r) export SCAN_ON_RELEASE=${OPTARG};;
 p) export SCAN_ON_PREVIEW=${OPTARG};;
+*) echo "usage: $0 [-s server] [-k token] [-r] [-p]"
 esac
 done
 
@@ -23,19 +25,20 @@ fi
 env
 ls -laR
 
-if [[ ! -f "sonar-project.properties" ]]; then
-    echo "Setting up default sonar-project.properties file for buildpack " ${BUILDPACK_NAME}
-    cp /sqproperties/${BUILDPACK_NAME}.sonar-project.properties sonar-project.properties || true
-fi
 if [[ -f "sonar-project.properties" ]]; then
-    cat sonar-project.properties
+    echo "Using sonar-project.properties file from project source app=jx-app-sonar-scanner sonarscanproperties=true"
 fi
+if [[ ! -f "sonar-project.properties" ]]; then
+    echo "Setting up default sonar-project.properties file for buildpack ${BUILDPACK_NAME}"
+    cp "/sqproperties/${BUILDPACK_NAME}.sonar-project.properties" sonar-project.properties || true
+fi
+
 # Only activate in preview builds or the first stage of a release
 if [[ ${IS_PREVIEW_PIPELINE} == "true" ]] || [[ ${IS_RELEASE_PIPELINE} == "true" ]] ; then
     if [[ ${IS_PREVIEW_PIPELINE} == "true" ]] ; then
         if [[ ${SCAN_ON_PREVIEW} == "true" ]] ; then
             echo "Sonarqube is scanning files..."
-            echo "BuildPack: " ${BUILDPACK_NAME}
+            echo "BuildPack: ${BUILDPACK_NAME}"
             /opt/sonar/bin/sonar-scanner "-Dsonar.host.url=${SONARQUBE_SERVER}" "-Dsonar.projectKey=${JOB_NAME}" "-Dsonar.login=${SONAR_TOKEN}" "-Dsonar.scm.provider=git"
         else
             echo "Sonarqube scanning disabled in preview builds."
@@ -44,7 +47,7 @@ if [[ ${IS_PREVIEW_PIPELINE} == "true" ]] || [[ ${IS_RELEASE_PIPELINE} == "true"
     if [[ ${IS_RELEASE_PIPELINE} == "true" ]] ; then
         if [[ ${SCAN_ON_RELEASE} == "true" ]] ; then
             echo "Sonarqube is scanning files..."
-            echo "BuildPack: " ${BUILDPACK_NAME}
+            echo "BuildPack: ${BUILDPACK_NAME}"
             /opt/sonar/bin/sonar-scanner "-Dsonar.host.url=${SONARQUBE_SERVER}" "-Dsonar.projectKey=${JOB_NAME}" "-Dsonar.login=${SONAR_TOKEN}" "-Dsonar.scm.provider=git"
         else
             echo "Sonarqube scanning disabled in release builds."
